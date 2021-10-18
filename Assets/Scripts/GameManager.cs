67781bacc98e1de4cb27853b361aa4d116d25701
salty-max <max@jellycat.fr>
2021-10-19 00:21:05 +0200

    Generates board and nodes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int _width = 4;
+    [SerializeField] private int _height = 4;
+    [SerializeField] private Node _nodePrefab;
+    [SerializeField] private SpriteRenderer _boardPrefab;
+
+    private void Start()
+    {
+        GenerateGrid();    
+    }
+
+    void GenerateGrid()
+    {
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                var node = Instantiate(_nodePrefab, new Vector2(x, y), Quaternion.identity);
+            }
+        }
+
+        var center = new Vector2((float)_width / 2 - 0.5f, (float)_height / 2 - 0.5f);
+
+        var board = Instantiate(_boardPrefab, center, Quaternion.identity);
+        board.size = new Vector2(_width, _height);
+
+        Camera.main.transform.position = new Vector3(center.x, center.y, -10);
+    }
+}
